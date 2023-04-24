26ce29f4be59efcb4c62f6c817dd1c6b386f9f95
LawlietAnime <ryusakilawlietgithub@gmail.com>
2023-04-24 21:42:38 +0200

    🎉 initial commit - code generation

---
+++ ChoutenTCA/ChoutenTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoutenTCAApp.swift
+//  ChoutenTCA
+//
+//  Created by Inumaki on 24.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ChoutenTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
