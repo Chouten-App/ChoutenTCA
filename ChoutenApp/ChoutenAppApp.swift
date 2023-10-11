799ee874ec6e623c42d24bdb5c032608a05b55cd
LawlietAnime <kempcompany05@gmail.com>
2023-10-11 11:09:34 +0200

    Initial Commit

---
+++ ChoutenApp/ChoutenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoutenAppApp.swift
+//  ChoutenApp
+//
+//  Created by Inumaki on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ChoutenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChoutenApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChoutenApp
+//
+//  Created by Inumaki on 11.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
