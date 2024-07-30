2496f7c314f793d67514ec60b2f969fcbe9b86ae
Eltik <76538547+Eltik@users.noreply.github.com>
2024-07-30 14:41:42 -0700

    feat: Collections base/home update

---
+++ Targets/Features/Home/Sources/HomeFeature.swift
@@ -0,0 +1,78 @@
+//
+//  HomeFeature.swift
+//  Discover
+//
+//  Created by Inumaki on 19.04.24.
+//
+
+import Architecture
+import Combine
+import RelayClient
+import DatabaseClient
+@preconcurrency import SharedModels
+import SwiftUI
+
+@Reducer
+public struct HomeFeature: Reducer {
+    @Dependency(\.relayClient) var relayClient
+    @Dependency(\.databaseClient) var databaseClient
+
+    @ObservableState
+    public struct State: FeatureState {
+        public var discoverSections: [DiscoverSection] = []
+
+        public init() { }
+    }
+
+    @CasePathable
+    @dynamicMemberLookup
+    public enum Action: FeatureAction {
+        @CasePathable
+        @dynamicMemberLookup
+        public enum ViewAction: SendableAction {
+            case onAppear
+            case setDiscoverSections(_ data: [DiscoverSection])
+        }
+
+        @CasePathable
+        @dynamicMemberLookup
+        public enum DelegateAction: SendableAction {}
+
+        @CasePathable
+        @dynamicMemberLookup
+        public enum InternalAction: SendableAction {}
+
+        case view(ViewAction)
+        case delegate(DelegateAction)
+        case `internal`(InternalAction)
+    }
+
+    public init() { }
+
+    @ReducerBuilder<State, Action> public var body: some ReducerOf<Self> {
+      Reduce { state, action in
+        switch action {
+        case let .view(viewAction):
+          switch viewAction {
+          case .onAppear:
+              state.discoverSections = []
+              return .merge(
+                  .run { send in
+                      do {
+                          try await self.databaseClient.initDB()
+                          
+                          let data = try await self.relayClient.discover()
+                          await send(.view(.setDiscoverSections(data)))
+                      } catch {
+                          print(error.localizedDescription)
+                      }
+                  }
+              )
+          case .setDiscoverSections(let data):
+              state.discoverSections = data
+              return .none
+          }
+        }
+      }
+    }
+}
---
+++ Targets/Shared/SharedModels/Sources/Home.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ian Lew on 7/30/24.
+//
+
+import Foundation
